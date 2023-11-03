2c3d24cd4f3f27679783b45f0953e6600f04ff60
Yingying Wang <yxw241@gmail.com>
2023-11-02 22:10:54 -0400

    styling-component

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import Figure from './Figure';
+import styled from 'styled-components';
 
+
+const StyleCard = styled.div`
+  border: 1px solid black;
+  border-radius:  12px;
+  padding: 2rem;
+
+  h2 {
+    color:  ${pr => pr.$color};
+    font-size: 2.5em;
+    margin: 0 0 1em 0;
+  }
+
+  p {
+    margin: 0 0 1.5rem 0;
+
+    &:: first-line {
+        font-size: 1.5em;
+    }
+  }
+      
+
+`
 export default function Card({title, text, imageURL, date}) {
     return (
-     <div className='card'> 
+     <StyleCard $color="orange" className='card'> 
         <h2>{title}</h2>
            <p>{text}</p>
        <Figure 
@@ -11,7 +34,7 @@ export default function Card({title, text, imageURL, date}) {
        caption={date}
        />
      
-     </div>
+     </StyleCard>
 
     )
 }
--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const StyledFigure = styled.figure`
+    max-width:  60%;
+
+    figcaption {
+         font-style: italic;
+    }
+   
+
+
+`
 
 
 export default function Figure({imageURL, caption}) {
     return (
-          <figure>
+          <StyledFigure>
           <img src={imageURL}    />
         <figcaption>Awesome pic taken on {caption}</figcaption>
-      </figure>
+      </StyledFigure>
  )
 }
